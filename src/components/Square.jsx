a51113af1387f829879615bcd6bd48e15dfe9c43
Vishal Srivastava <eth.vishal@gmail.com>
2023-07-18 13:51:54 +0530

    Added virbation effect on touch

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,18 @@
-
-
 function Square(props) {
   return (
-    <div  className="square border border-5 rounded-4 border-primary " onClick={()=>props.onClick(props.index)} ><h1>{props.value}</h1></div>
-  )
+    <div
+      className="square border border-5 rounded-4 border-primary "
+      onClick={() => {
+        if("vibrate" in navigator){
+          console.log('I got clicked.')
+          navigator.vibrate(200);
+        }
+        props.onClick(props.index);
+      }}
+    >
+      <h1>{props.value}</h1>
+    </div>
+  );
 }
 
-export default Square
+export default Square;
